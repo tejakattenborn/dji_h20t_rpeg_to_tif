56b5acbe7cb8ecc6a784d5d014404ed673369dba
tejakattenborn <38572828+tejakattenborn@users.noreply.github.com>
2023-06-02 14:52:50 +0200

    Update dji_h20t_rpeg_to_tif_v1.r

--- dji_h20t_rpeg_to_tif_v1.r
+++ dji_h20t_rpeg_to_tif_v1.r
@@ -40,31 +40,37 @@ in_files = list.files(in_dir, full.names = T, pattern = "_T")
 ### calibration/conversion procedure (...could be paralleled)
 for(i in 1:length(in_files)){
   
-  # calibration to celsius
-  in_exif = exifr::read_exif(in_files[i])
-  in_name = in_files[i]
-  out_name = paste0(out_dir, substr(basename(in_files[i]), 0, nchar(basename(in_files[i]))-4), ".raw")
-  run(paste0("dji_irp.exe -s ", in_name, " -a measure -o ", out_name, " --measurefmt float32", 
-             " --emissivity ", emissivity, " --humidity ", humidity, " --distance ", distance))
-  
-  # from .raw (hex) to .tif (celsius in float)
-  raw_data <- readBin(out_name, "double", size = 4, n = in_exif$ImageWidth*in_exif$ImageHeight)
-  image_matrix <- matrix(raw_data, nrow = in_exif$ImageHeight, ncol = in_exif$ImageWidth, byrow = T)
-  out_name_tif = paste0(substr(out_name, 0, nchar(out_name)-4), ".tif")
-  write_tif(image_matrix, path = out_name_tif, overwrite = TRUE)
-  
-  # transfer metadata (exif)
-  exiftool_call(paste0("-Model=", in_exif$Model[1]), out_name_tif)
-  exiftool_call(paste0("-Make=", in_exif$Make[1]), out_name_tif)
-  exiftool_call(paste0("-Orientation=", in_exif$Orientation[1]), out_name_tif)
-  exiftool_call(paste0("-FocalLength=", in_exif$FocalLength[1]), out_name_tif)
-  exiftool_call(paste0("-FocalLengthIn35mmFormat=", in_exif$FocalLengthIn35mmFormat[1]), out_name_tif)
-  exiftool_call(paste0("-DigitalZoomRatio=", in_exif$DigitalZoomRatio[1]), out_name_tif)
-  exiftool_call(paste0("-ApertureValue=", in_exif$ApertureValue[1]), out_name_tif)
-  exiftool_call(paste0("-GPSAltitude=", in_exif$GPSAltitude[1]), out_name_tif)
-  exiftool_call(paste0("-GPSLatitude=", in_exif$GPSLatitude[1]), out_name_tif)
-  exiftool_call(paste0("-GPSLongitude=", in_exif$GPSLongitude[1]), out_name_tif)
-  exiftool_call(paste0("GPSLatitudeRef=", in_exif$GPSLatitudeRef[1]), out_name_tif)
+  # Some images may be overexposed. The following lines prevent the loop from stopping in such case (and there will be no putput).
+  # Test to reduce the humidty parameter if too many images are missing after the loop.
+  tryCatch({
+    
+    # calibration to celsius
+    in_exif = exifr::read_exif(in_files[i])
+    in_name = in_files[i]
+    out_name = paste0(out_dir, substr(basename(in_files[i]), 0, nchar(basename(in_files[i]))-4), ".raw")
+    run(paste0("dji_irp.exe -s ", in_name, " -a measure -o ", out_name, " --measurefmt float32", 
+               " --emissivity ", emissivity, " --humidity ", humidity, " --distance ", distance))
+    
+    # from .raw (hex) to .tif (celsius in float)
+    raw_data <- readBin(out_name, "double", size = 4, n = in_exif$ImageWidth*in_exif$ImageHeight)
+    image_matrix <- matrix(raw_data, nrow = in_exif$ImageHeight, ncol = in_exif$ImageWidth, byrow = T)
+    out_name_tif = paste0(substr(out_name, 0, nchar(out_name)-4), ".tif")
+    write_tif(image_matrix, path = out_name_tif, overwrite = TRUE)
+    
+    # transfer metadata (exif)
+    exiftool_call(paste0("-Model=", in_exif$Model[1]), out_name_tif)
+    exiftool_call(paste0("-Make=", in_exif$Make[1]), out_name_tif)
+    exiftool_call(paste0("-Orientation=", in_exif$Orientation[1]), out_name_tif)
+    exiftool_call(paste0("-FocalLength=", in_exif$FocalLength[1]), out_name_tif)
+    exiftool_call(paste0("-FocalLengthIn35mmFormat=", in_exif$FocalLengthIn35mmFormat[1]), out_name_tif)
+    exiftool_call(paste0("-DigitalZoomRatio=", in_exif$DigitalZoomRatio[1]), out_name_tif)
+    exiftool_call(paste0("-ApertureValue=", in_exif$ApertureValue[1]), out_name_tif)
+    exiftool_call(paste0("-GPSAltitude=", in_exif$GPSAltitude[1]), out_name_tif)
+    exiftool_call(paste0("-GPSLatitude=", in_exif$GPSLatitude[1]), out_name_tif)
+    exiftool_call(paste0("-GPSLongitude=", in_exif$GPSLongitude[1]), out_name_tif)
+    exiftool_call(paste0("GPSLatitudeRef=", in_exif$GPSLatitudeRef[1]), out_name_tif)
+    
+  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
 }
 
 ### remove temp files
